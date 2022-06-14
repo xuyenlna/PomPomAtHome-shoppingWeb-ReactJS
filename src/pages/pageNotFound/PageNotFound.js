e10d59eb4f6896c9df9d371e994ca500d4e89c39
xuyenlna <anhxuyenln@gmail.com>
2022-06-14 11:27:18 +0700

    pageNotFound

--- src/pages/pageNotFound/PageNotFound.js
+++ src/pages/pageNotFound/PageNotFound.js
@@ -1,5 +1,38 @@
 import React from "react";
 
 export default function PageNotFound() {
-  return <div>PageNotFound</div>;
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        marginTop: "50px",
+      }}
+    >
+      <p
+        style={{
+          fontSize: "40px",
+        }}
+      >
+        404 PAGE NOT FOUND!
+      </p>
+      <p
+        style={{
+          fontSize: "18px",
+        }}
+      >
+        The page you requested does not exist. Click{" "}
+        <a
+          href="/"
+          style={{
+            fontSize: "18px",
+            textDecoration: "underline",
+            color: "#3a434d",
+          }}
+        >
+          here
+        </a>{" "}
+        to continue shopping.
+      </p>
+    </div>
+  );
 }
